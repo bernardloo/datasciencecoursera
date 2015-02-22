09f7bee8218a2139836fdbb64eac2d853ab0eecd
Bernard Loo <bernardloo@yahoo.com.sg>
2015-02-22 14:44:01 +0800

    Version1A
    initial

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,17 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-}
+a <- makeCacheMatrix( matrix(c(1,2,12,13), nrow = 2, ncol = 2) );
 
-
-
+#makeCacheMatrix <- function(x = matrix()) {
+#
+#}
 
 
 ## Write a short comment describing this function
-
 
 cacheSolve <- function(x, ...) {
-
-## Return a matrix that is the inverse of 'x'
-
+        ## Return a matrix that is the inverse of 'x'
 }
